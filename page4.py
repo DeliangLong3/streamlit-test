6d0faa7ebecb79f0f107adba78998e749464f88a
DeliangLong3 <372538056@qq.com>
2025-10-14 11:53:17 +0800

    Add files via upload

--- page4.py
+++ page4.py
@@ -3,25 +3,30 @@ import streamlit as st
 with st.sidebar:
     name=st.text_input("请输入你的名字")
     if name:
-        st.write(f"你好,{name}，我们周末要去迪士尼噢")
+        st.write(f"你好,{name}，谢谢你参与测试")
 
 
 column1,column2,column3=st.columns([2,2,1])
 with column1:
-    password=st.text_input("请输入你的密码：", type="password")
+    password=st.text_input("你能打开这个网页吗？：")
+    if password:
+        st.write(f"谢谢你的尝试，请继续回答下一个问题。")
 
 with column2:
-    paragraph=st.text_area("请输入一段自我介绍")
+    paragraph=st.text_area("你觉得这个网页看起来简单吗？")
+    if paragraph:
+        st.write(f"看来你认为这个网页挺{paragraph}嘛！😄")
 
 with column3:
-    age=st.number_input("请输入你的年龄：",value=20,min_value=0,max_value=150,step=5)
-st.write(f"你的年龄是：{age}岁")
+    score=st.number_input("请给这个网页打个分数吧：",value=None,max_value=100,step=10)
+    if score:
+        st.write(f"你打的分数是：{score}分，谢谢你的评估和打分。")
 
 st.divider()
 
-checked=st.checkbox("同意以上条款")
+checked=st.checkbox("已完成本网页测评。")
 if checked:
-    st.write("感谢你的同意")
+    st.write("感谢你的测评")
 
 st.divider()
 
